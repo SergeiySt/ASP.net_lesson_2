080736b41e012f6add3e69d0b06d8b7899e84a53
SergeiySt <ser.st.1989@gmail.com>
2023-10-05 20:59:01 +0300

    add_2

---
+++ Vecherunka_ASP_net/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vecherunka_ASP_net.Models;
+
+namespace Vecherunka_ASP_net.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ViewResult RsvpPost(GuestResponse guest) 
+        {
+            return View("Thanks", guest);
+        }
+            
+        
+    }
+}
